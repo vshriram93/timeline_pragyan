a3993cbcc820f361cdb8cfa64cd8db84c6136826
vshriram93 <steve@localhost.localdomain>
2013-11-24 02:24:52 +0530

    a big push

--- index.php
+++ index.php
@@ -7,13 +7,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-touch-fullscreen" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <style>
-      html, body {
-       height:100%;
-       padding: 0px;
-       margin: 0px;
-      }
-    </style>
+    <link href="css/pragyan-timeline.css" rel="stylesheet" type="text/css" />
     <!--[if lt IE 9]>
        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
@@ -21,16 +15,17 @@
   <body>
       <div id="header">
          <div id="headerimg">
-           <img src="images/header.jpg" alt="header"/>
+           <img src="images/logo.png" alt="header"/>
          </div>
       </div>
       <!-- BEGIN Timeline Embed -->
       <div id="timeline-embed"></div>
       <script type="text/javascript">
+        var screenHeight = screen.height-92+'px';
         var timeline_config = {
          width: "100%",
-         height: "100%",
-         source: 'example_json.json'
+         height: "80%",
+         source: 'json/pragyan_timeline.json'
         }
       </script>
       <script type="text/javascript" src="js/storyjs-embed.js"></script>
